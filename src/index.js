b1e0c694b1cd5cacf2a0da7659b161c83c889d52
victornpb <3372598+victornpb@users.noreply.github.com>
2022-04-24 12:22:11 -0300

    WeakMatrix implememtation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import sum from './utils/sum';
 
-export default class FooBar {
-  str = 'Hello World';
-  #p = 1;
-  constructor() {
-    console.log('FooBar' + this.str + this.#p);
+// bi-dimensional WeakMap implementation
+export default class WeakMatrix {
+  #map = new WeakMap();
+
+  set(key1, key2, value) {
+    let map2 = this.#map.get(key1);
+    if (!map2) {
+      map2 = new WeakMap();
+      this.#map.set(key1, map2);
+    }
+    map2.set(key2, value);
   }
-  sum(a, b) {
-    return sum(a, b);
+
+  get(key1, key2) {
+    return this.#map.get(key1)?.get(key2);
   }
-  #bar() {
-    return 123;
+
+  has(key1, key2) {
+    const map = this.#map.get(key1);
+    if (!map) return false;
+    return map.has(key2);
+  }
+
+  delete(key1, key2) {
+    const map = this.#map.get(key1);
+    if (!map) return false;
+    return map.delete(key2);
+  }
+
+  clear(key1) {
+    if (key1!==undefined) this.#map.get(key1)?.clear();
+    else this.#map.clear();
   }
 }
+
+const m = new WeakMap();
